4aa0d3950dee9222e8c38b03dafe7704171e18d7
trannghiatvt108 <85309707+trannghiatvt108@users.noreply.github.com>
2021-06-04 19:49:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Khủng Long à!',
+    introTitle: 'Hello Khủng Long!',
     introDesc: `Em không phải là cướp
     Cớ sao em lại 
     cướp mất
     tim anh`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã thích ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy anh ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Em đang làm gì đấy? 😙',
+    desc: 'À nhầm! em thấy anh có dễ thương không? hihi ',
+    btnYes: 'Nhất quả đất nhé <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
+    question:'Vậy là thích anh đúng hông? <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Thích thì thích mà không thích thì yêu <33333333',
+    reply: 'Thích thì thích mà không thích thì thích <33333333',
     mess: 'Anh biết mà 🥰. me toooooo 😘😘',
-    messDesc: 'Qua dịch đi chơi nhé!.',
+    messDesc: 'Trà Sữa em eiiiiii!.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/soserious1096/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
